20cac846ba7c747a3ee48db1d25ae5b877dec43c
nemzki <maglawaymark017@gmail.com>
2025-11-20 19:30:22 +0800

    feat/create-table-invoices

--- veterinary.sql
+++ veterinary.sql
@@ -39,3 +39,12 @@ CREATE TABLE doctors(
     phone VARCHAR(15),
     email VARCHAR(100)
 );
+
+CREATE TABLE invoices
+(
+    invoiceid INT PRIMARY KEY,
+    appointid INT,
+    totalamount NUMERIC(10,2),
+    paymentdate TIME,
+    FOREIGN KEY(appointid) REFERENCES appointments(appointid)
+);
