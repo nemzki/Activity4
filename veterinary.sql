af8f7f671397dd18d0e90182cba658a6d44c15e3
nemzki <maglawaymark017@gmail.com>
2025-11-20 19:33:03 +0800

    feat/insert-10-rows-owners

--- veterinary.sql
+++ veterinary.sql
@@ -60,3 +60,15 @@ CREATE TABLE medicalrecords(
     FOREIGN KEY(animalid) references animals(animalid),
     FOREIGN KEY(doctorid) references doctors(doctorid)
 );
+
+INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email)
+VALUES (1, 'Jim John', 'Ebreo', 'Rosario', 09123456789, 'ebreo@gmail.com'),
+        (2, 'Shenna', 'Bellen', 'Japan', 09223456789, 'bellen@gmail.com'),
+        (3, 'Gee-Anne', 'Lescano', 'Catalina', 09323456789, 'lescano@gmail.com'),
+        (4, 'Rona', 'Silva', 'Sico', 09423456789, 'silva@gmail.com'),
+        (5, 'John', 'Doe', 'Cavite', 09523456789, 'doe@gmail.com'),
+        (6, 'Hannah', 'Cuevas', 'Cuenca', 09623456789, 'cuevas@gmail.com'),
+        (7, 'Monica', 'Pornela', 'BSU', 09723456789, 'pornela@gmail.com'),
+        (8, 'Ambross', 'Matibag', 'Taga-Gubat', 09823456789, 'matibag@gmail.com'),
+        (9, 'Angela', 'Dalangin', 'San Jose', 09923456789, 'dalangin@gmail.com'),
+        (10, 'Jhon Rhey', 'Tenorio', 'Taga sa kanila', 09133456789, 'tenors@gmail.com');
